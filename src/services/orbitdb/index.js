995c0e3fcaa431cbd80415431bf86b93926b4340
alifadlie <syedfadlie@gmail.com>
2020-09-04 12:08:38 +1000

    init orbit put

--- src/services/orbitdb/index.js
+++ src/services/orbitdb/index.js
@@ -6,5 +6,25 @@ export const dbService = async () => {
   // Create orbitdb
   const orbitdb = await OrbitDb.createInstance(ipfs)
   const db = await orbitdb.keyvalue('first-database')
+
+  //Test to add and retreive a KV
+  db.put('twenty', '20').then( () => {
+    console.log(db.get('twenty'))
+    // 20
+  })
+
+  //Adding mock KV data
+  await db.put('one', '1')
+  await db.put('two', '2')
+  await db.put('three', '3')
+
+  //Set const variable to hold all entries
+  const out = db.all
+
+  //Test updating a KV data by using a 'put' alias called 'set'
+  await db.set('three', 'thr33')
+
+  //Output all entries
+  console.log(out)
   console.log(db.address.toString())
 };
