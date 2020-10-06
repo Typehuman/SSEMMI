9fe246d3670e4995562ee33e8e69f9a6089144b7
alifadlie <syedfadlie@gmail.com>
2020-10-06 12:33:15 +1100

    setup auth using yeoman generator

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import http from 'http'
 import { env, mongo, port, ip, apiRoot } from './config'
+import mongoose from './services/mongoose'
 import express from './services/express'
+import api from './api'
 import { dbService, getAll, getItem, post } from './services/orbitdb'
 
 import api from './api/routes'
 
 const app = express(apiRoot, api)
 const server = http.createServer(app)
+
+//Using the routes from API to handle endpoints
+app.use("/", api)
+
+if (mongo.uri) {
+  mongoose.connect(mongo.uri)
+}
+mongoose.Promise = Promise
+
 // dbService()
 
 setImmediate(() => {
@@ -14,8 +25,5 @@ setImmediate(() => {
     console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
   })
 })
-
-//Using the routes from API to handle endpoints
-app.use("/", api)
 
 export default app
