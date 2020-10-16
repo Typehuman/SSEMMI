bf84f1f26f3f653b6818ba9e1966629c95b41a65
alifadlie <syedfadlie@gmail.com>
2020-10-16 13:19:42 +1100

    barebone design

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginPage from './components/Pages/LoginPage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
 }
 </script>
---
+++ src/client/src/components/Pages/LoginPage.vue
@@ -0,0 +1,33 @@
+// LOGIN PAGE
+
+<template>
+  <div class="LoginPage">
+      <h1>SSEMMI API</h1>
+    <legend>Login Page</legend>
+    <!-- Form -->
+    <form>
+        <!-- Email address input -->
+        <div class="form-group">
+            <label>Username</label>
+            <input type="username" class="form-control form-control-lg"/>
+        </div>
+        <!-- Password input -->
+        <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control form-control-lg"/>
+        </div>
+    </form>
+    <button>Submit</button>
+    <button>Forgot my password</button>
+    <button>Register for a new account</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .LoginPage {
+    text-align: center;
+  }
+</style>
