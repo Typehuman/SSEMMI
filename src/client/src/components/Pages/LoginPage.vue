b55a631945a75724e6a7e111ff19821b7ebf3551
alifadlie <syedfadlie@gmail.com>
2020-10-16 13:42:19 +1100

    login revamp

--- src/client/src/components/Pages/LoginPage.vue
+++ src/client/src/components/Pages/LoginPage.vue
@@ -1,33 +1,118 @@
 // LOGIN PAGE
 
 <template>
-  <div class="LoginPage">
-      <h1>SSEMMI API</h1>
-    <legend>Login Page</legend>
-    <!-- Form -->
-    <form>
-        <!-- Email address input -->
-        <div class="form-group">
-            <label>Username</label>
-            <input type="username" class="form-control form-control-lg"/>
-        </div>
-        <!-- Password input -->
-        <div class="form-group">
-            <label>Password</label>
-            <input type="password" class="form-control form-control-lg"/>
-        </div>
-    </form>
-    <button>Submit</button>
-    <button>Forgot my password</button>
-    <button>Register for a new account</button>
+<div class="container" id='app'>
+  <div class="login">
+    <header class="login--header">
+      <h1>SSEMMI CLIENT</h1>
+      <span>LOGIN</span>
+    </header>
+    <section class="login--section">
+      <form class='login--form'>
+        <fieldset>
+          <input type="text" placeholder='Username' required />
+        </fieldset>
+        <fieldset>
+          <input type="password" placeholder='Password' required/>
+        </fieldset>
+        <fieldset>
+          <button type='submit' class='btn'>Submit</button>
+        </fieldset>
+      </form>
+    </section>
   </div>
+</div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
-  .LoginPage {
-    text-align: center;
-  }
+  * {
+  margin: 0;
+  padding: 0;
+  font-family: 'Source Sans Pro', sans-serif;
+  line-height: 1.5;
+}
+
+body {
+  background: #607D8B;
+}
+
+body, input, button {
+  font-size: 1.2rem;
+}
+
+fieldset {
+  border: none;
+}
+
+.container {
+  background: none;
+  width: 400px;
+  margin: 4rem auto 0;
+  text-align: center;
+  box-shadow: 0 1rem 1rem 0 rgba(0, 0, 0, .15);
+  position: relative;
+}
+
+.login {
+  z-index: 1;
+  position: relative;
+  background: white;
+  padding: .75rem 1.5rem 1.5rem;
+  box-sizing: border-box;
+}
+
+.login--header {
+  margin-bottom: 1rem;
+}
+
+.login--header span {
+  font-size: 1rem;
+}
+
+.btn {
+  background: white;
+  box-shadow: inset 0 0 2px 0 #EEEEEE;
+  outline: none;
+  border: 1px solid #3F51B5;
+  padding: .3rem 1rem .4rem;
+  cursor: pointer;
+  border-radius: .25rem;
+  margin-top: 1rem;
+  color: #3F51B5;
+}
+
+.btn:active {
+  box-shadow: inset 2px 2px 2px 0 #E0E0E0;
+}
+
+input {
+  width: 100%;
+  text-align: center;
+  outline: none;
+  padding: .5rem 1rem;
+  box-sizing: border-box;
+  background: none;
+}
+
+.line {
+  transform: translate(0, -1rem);
+  stroke-width: .5;
+}
+
+.line--default {
+  stroke: #ccc;
+  transition: all .2s ease-out;
+}
+
+input:focus + svg > .line--default {
+  stroke: #3F51B5;
+}
+
+input:focus:invalid + svg > .line--default {
+  stroke: #FF5722;
+}
+
 </style>
