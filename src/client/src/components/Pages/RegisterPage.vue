e953db4fbe467f5ccf6a2b8f2ec5087a4e0240c1
alifadlie <syedfadlie@gmail.com>
2020-10-16 14:02:00 +1100

    using mdbvue for design library and init register page

---
+++ src/client/src/components/Pages/RegisterPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <mdb-container>
+    <mdb-row>
+      <mdb-col size="12" class="text-center mb-5">
+        <mdb-btn @click.native="showModal = true" color="info">Launch modal contact form</mdb-btn>
+        <mdb-modal :show="showModal" @close="showModal = false" cascade class="text-left">
+          <mdb-modal-header class="primary-color white-text">
+            <h4 class="title"><fa class="fas fa-pencil-alt" /> Contact form</h4>
+          </mdb-modal-header>
+          <mdb-modal-body class="grey-text">
+            <mdb-input size="sm" label="Your name" icon="user" group type="text" validate error="wrong" success="right"/>
+            <mdb-input size="sm" label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+            <mdb-input size="sm" label="Subject" icon="tag" group type="text" validate error="wrong" success="right"/>
+            <mdb-textarea size="sm" :rows="2" label="Your message" icon="pencil"/>
+          </mdb-modal-body>
+          <mdb-modal-footer>
+            <mdb-btn color="secondary" @click.native="showModal = false">Close</mdb-btn>
+            <mdb-btn color="primary">Save changes</mdb-btn>
+          </mdb-modal-footer>
+        </mdb-modal>
+      </mdb-col>
+    </mdb-row>
+  </mdb-container>
+</template>
+
+<script>
+  import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbTextarea, mdbBtn, mdbModal, mdbModalHeader, mdbModalBody, mdbModalFooter } from 'mdbvue';
+  export default {
+    name: 'FormsPage',
+    components: {
+      mdbContainer,
+      mdbRow,
+      mdbCol,
+      mdbInput,
+      mdbTextarea,
+      mdbBtn,
+      mdbModal,
+      mdbModalHeader,
+      mdbModalBody,
+      mdbModalFooter
+    },
+    data() {
+      return {
+        showModal: false
+      };
+    }
+  }
+</script>
+
