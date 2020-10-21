23bac12b4f407669a8fbf5077245417f9f9b05b1
alifadlie <syedfadlie@gmail.com>
2020-10-21 15:48:55 +1100

    register ui to send request payload to server. Vue to serve on alt port

--- src/client/src/components/Pages/RegisterPage.vue
+++ src/client/src/components/Pages/RegisterPage.vue
@@ -1,42 +1,71 @@
 <template>
-  <div>
-  
-        <mdb-btn @click.native="showModal = true" color="info">New and want to register?</mdb-btn>
-        <mdb-modal :show="showModal" @close="showModal = false" cascade class="text-left">
-          <mdb-modal-header class="primary-color white-text title">
-            <h4>Register a new account</h4>
-          </mdb-modal-header>
-          <mdb-modal-body class="grey-text">
-            <mdb-input size="sm" label="Your name" icon="user" group type="text" validate error="wrong" success="right"/>
-            <mdb-input size="sm" label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
-            <mdb-input size="sm" label="Your password" icon="key" group type="text" validate error="wrong" success="right"/>
-          </mdb-modal-body>
-          <mdb-modal-footer>
-            <mdb-btn color="secondary" @click.native="showModal = false">Close</mdb-btn>
-            <mdb-btn color="primary">Save changes</mdb-btn>
-          </mdb-modal-footer>
-        </mdb-modal>
-
-  </div>
+<div>
+  <!-- Title and header on the UI -->
+    <header class="register--header">
+      <h1>SSEMMI CLIENT</h1>
+      <span>Create User</span>
+    </header>
+    <section class="register--section">
+      <!-- UI for passing register details -->
+      <form class='register--form' @submit="login">
+        <fieldset>
+          <mdb-input placeholder="email" name="Email" icon="envelope" type="email" required/>
+        </fieldset>
+        <fieldset>
+          <mdb-input placeholder="confirm email" v-model.trim="registerUserData.email" name="Confirm email" icon="exclamation-triangle" type="text" required/>
+        </fieldset>
+        <fieldset>
+          <mdb-input placeholder="password" v-model.trim="registerUserData.password" name="Password" icon="lock" type="password" required/>
+        </fieldset>
+        <fieldset>
+          <mdb-btn type='submit' color="primary">Register User</mdb-btn>
+        </fieldset>
+      </form>
+    </section>
+</div>
 </template>
 
 <script>
-  import { mdbInput, mdbBtn, mdbModal, mdbModalHeader, mdbModalBody, mdbModalFooter } from 'mdbvue';
-  export default {
-    name: 'Register',
-    components: {
-      mdbInput,
-      mdbBtn,
-      mdbModal,
-      mdbModalHeader,
-      mdbModalBody,
-      mdbModalFooter
-    },
-    data() {
-      return {
-        showModal: false
-      };
+import { mdbInput, mdbBtn } from 'mdbvue';
+import axios from 'axios';
+
+export default {
+  name: 'Register',
+  components: {
+    mdbInput,
+    mdbBtn
+  },
+  data() {
+    return {
+      registerUserData: {}
+    };
+  },
+  methods: {
+    register(event) {
+      // Check for event error to prevent propagation
+      event.preventDefault()
+
+      const regUserRequst = {
+        'email': this.registerUserData.email,
+        'password': this.registerUserData.password,
+        'access_token': process.env.MASTER_KEY
+      }
+
+      //Header post method to pass user details by passing login details
+      axios.post('http://localhost:9000/users/', regUserRequst)
+      // Retreive token and redirect to requested page
+      .then( regUser => {
+        // localStorage.setItem('access_token', user.data.token)
+        console.log('authenticadddddd')
+        console.log(regUser.data)
+        this.$router.push({name: 'Dashboard'})
+      })
+      // Check for request errors
+      .catch(err => {
+        console.log(err)
+      })
     }
   }
+}
 </script>
 
