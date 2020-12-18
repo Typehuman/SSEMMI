ab48dcd2a9859c24e2e817d571e0c6a257e3465a
alifadlie <syedfadlie@gmail.com>
2020-12-18 14:11:04 +1100

    adding a card component to create frame for map

--- src/client/src/components/Pages/VisualiserPage.vue
+++ src/client/src/components/Pages/VisualiserPage.vue
@@ -1,14 +1,27 @@
 <template>
-    <Map />
+    <div style="card">
+        <CardComponent componentHeader="Sightings Map">
+            <Map />
+        </CardComponent>
+    </div>
 </template>
 
 <script>
+import CardComponent from '../CardComponent'
 import Map from '../MapComponent'
 
 export default {
     name: 'Visualiser',
     components: {
+        CardComponent,
         Map
     },
 }
 </script>
+
+<style>
+.card {
+    left: 50%;
+    top: 5%;
+}
+</style>
