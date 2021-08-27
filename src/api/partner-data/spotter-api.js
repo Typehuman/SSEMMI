f32a336ea563aad890ffa14d645c89a31c3a4fff
Peter Ince <pdince@gmail.com>
2021-08-27 10:03:22 +1000

    remove species specifier

--- src/api/partner-data/spotter-api.js
+++ src/api/partner-data/spotter-api.js
@@ -25,9 +25,9 @@ currentDate = String(currentDate)
 // Formatting on current date for API parameter
 var currentDayFormat = `${yyyy}-${mm}-${dd}`
 // Current day format to display sightings since the current year
-export const conserveApi = `https://maplify.com/waseak/php/search-all-sightings.php?&BBOX=-180,0,180,90&start=${currentDayFormat}&species=Orcinus%20orca`
+export const conserveApi = `https://maplify.com/waseak/php/search-all-sightings.php?&BBOX=-180,0,180,90&start=${currentDayFormat}`
 
-const conHistory = 'https://maplify.com/waseak/php/search-all-sightings.php?&BBOX=-180,0,180,90&start=2020-01-01&species=Orcinus%20orca'
+const conHistory = 'https://maplify.com/waseak/php/search-all-sightings.php?&BBOX=-180,0,180,90&start=2020-01-01'
 
 // Retreive data from the URL
 export const loadApi = async (api, loadHistory = false) => {
